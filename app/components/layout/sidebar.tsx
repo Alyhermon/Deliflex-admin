48b10a79a29e2c6d4e4f9f6d39d303c8135383ae
alyhermon <Alinahermon11@gmail.com>
2026-04-30 21:04:59 -0400

    feat: Add 'Inventario' menu item to Sidebar with corresponding icon

--- app/components/layout/sidebar.tsx
+++ app/components/layout/sidebar.tsx
@@ -11,6 +11,7 @@ import {
   faGear,
   faUser,
   faCircleUser,
+  faBox,
 } from "@fortawesome/free-solid-svg-icons";
 import { ReactNode } from "react";
 import styles from "./sidebar.module.css";
@@ -22,14 +23,47 @@ interface MenuItem {
 }
 
 const menuItems: MenuItem[] = [
-  { name: "Dashboard", path: "/dashboard", icon: <FontAwesomeIcon icon={faChartColumn} /> },
-  { name: "Negocios", path: "/stores", icon: <FontAwesomeIcon icon={faShop} /> },
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+    icon: <FontAwesomeIcon icon={faChartColumn} />,
+  },
+  {
+    name: "Negocios",
+    path: "/stores",
+    icon: <FontAwesomeIcon icon={faShop} />,
+  },
   { name: "Menú", path: "/menu", icon: <FontAwesomeIcon icon={faBarcode} /> },
-  { name: "Operaciones", path: "/operaciones", icon: <FontAwesomeIcon icon={faChartLine} /> },
-  { name: "Finanzas", path: "/finanzas", icon: <FontAwesomeIcon icon={faCalculator} /> },
-  { name: "Usuarios y Roles", path: "/users-rols", icon: <FontAwesomeIcon icon={faUser} /> },
-  { name: "Configuraciones", path: "/configuracion", icon: <FontAwesomeIcon icon={faGear} /> },
-  { name: "Login", path: "core/login", icon: <FontAwesomeIcon icon={faCircleUser} /> },
+  {
+    name: "Inventario",
+    path: "/inventory",
+    icon: <FontAwesomeIcon icon={faBox} />,
+  },
+  {
+    name: "Operaciones",
+    path: "/operaciones",
+    icon: <FontAwesomeIcon icon={faChartLine} />,
+  },
+  {
+    name: "Finanzas",
+    path: "/finanzas",
+    icon: <FontAwesomeIcon icon={faCalculator} />,
+  },
+  {
+    name: "Usuarios y Roles",
+    path: "/users-rols",
+    icon: <FontAwesomeIcon icon={faUser} />,
+  },
+  {
+    name: "Configuraciones",
+    path: "/configuracion",
+    icon: <FontAwesomeIcon icon={faGear} />,
+  },
+  {
+    name: "Login",
+    path: "core/login",
+    icon: <FontAwesomeIcon icon={faCircleUser} />,
+  },
 ];
 
 export default function Sidebar() {
