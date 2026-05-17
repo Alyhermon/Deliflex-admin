48a71de0399fb403fd057e80395a2eb2c2b630b9
alyhermon <Alinahermon11@gmail.com>
2026-05-16 23:12:14 -0400

    feat: Implement login functionality with user role validation and token storage

--- app/core/login/page.tsx
+++ app/core/login/page.tsx
@@ -9,6 +9,49 @@ import { useState } from "react";
 export default function DashboardPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+
+  const login = async () => {
+  try {
+    const response = await fetch('http://localhost:3000/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      alert(data.message);
+      return;
+    }
+    //estoy cansada por hoy seguimos mañana 17/05/2026
+
+    // validar admin
+    if (data.user.global_role_id !== 90) {
+      alert('No tienes acceso al panel administrador');
+      return;
+    }
+
+    // guardar token
+    localStorage.setItem('token', data.access_token);
+
+    // guardar usuario
+    localStorage.setItem('user', JSON.stringify(data.user));
+
+    // redireccionar
+    router.push('/dashboard');
+
+  } catch (error) {
+    console.error(error);
+  }
+};
+
   return (
     <div className={styles.container}>
       <Image src={"/assets/fondo.png"} alt="banner" fill />
