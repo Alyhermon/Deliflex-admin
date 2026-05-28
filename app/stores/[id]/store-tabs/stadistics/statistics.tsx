46c85bf6e179f2e12c2440845ce34a4b808ec191
alyhermon <Alinahermon11@gmail.com>
2026-05-27 22:42:04 -0400

    fix: Update footer comment for clarity in DashboardPage

--- app/stores/[id]/store-tabs/stadistics/statistics.tsx
+++ app/stores/[id]/store-tabs/stadistics/statistics.tsx
@@ -376,8 +376,8 @@ export default function DashboardPage() {
         </div>
       </div>
 
-FOOTER
-
+      {/* FOOTER */}
+      {/*Vengo ahora*/}
       <div className={styles.footer}>
         ● Los datos se actualizan cada 30 minutos
       </div>
